6ed140e56f3e11e4de934af13ed328e277d6a544
Rupam Laha <rupamlaha9@gmail.com>
2020-05-11 01:48:42 +0530

    Adding From PhotoLibrary option

--- SimpleCameraApp/ViewController.swift
+++ SimpleCameraApp/ViewController.swift
@@ -11,34 +11,71 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var removePhoto: UIButton!
     
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imagePicker.sourceType = UIImagePickerController.SourceType.camera
-        imagePicker.delegate = self
+        removePhotoButtonVisibility()
         
     }
-
+    
+    func removePhotoButtonVisibility(){
+        
+        if image.image != nil {
+                   removePhoto.isHidden = false
+               }
+        else{
+            removePhoto.isHidden = true
+        }
+    }
+    
+    @IBAction func removePhotoAction(_ sender: Any) {
+        
+        image.image = nil
+        removePhoto.isHidden = true
+        
+    }
+    
     @IBAction func clickPictureAction(_ sender: Any) {
         
+        imagePicker.sourceType = UIImagePickerController.SourceType.camera
+        imagePicker.delegate = self
         present(imagePicker, animated: true, completion: nil)
     }
     
+    @IBAction func fromGelleryAction(_ sender: Any) {
+        
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
-        picker.dismiss(animated: true, completion: nil)
-        
-        image.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-        
+        if imagePicker.sourceType == UIImagePickerController.SourceType.camera{
+            picker.dismiss(animated: true, completion: nil)
+            image.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            imagePicker.delegate = nil
+            self.removePhotoButtonVisibility()
+        }
+        else{
+            
+            picker.dismiss(animated: true, completion: nil)
+            image.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            imagePicker.delegate = nil
+            self.removePhotoButtonVisibility()
+        }
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         
         picker.dismiss(animated: true, completion: nil)
-        print("Camera has been canceled")
+//        print("Camera has been canceled")
     }
     
 }
